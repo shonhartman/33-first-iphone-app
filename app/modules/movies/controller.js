db5094633e420176a1f319a07c8d5ba1e437afd4
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-04-26 15:37:02 -0500

    working

--- app/modules/movies/controller.js
+++ app/modules/movies/controller.js
@@ -1,6 +1,28 @@
 class MoviesController {
-  constructor(MoviesService, $ionicModal, $scope) {
+  constructor(MoviesService, $ionicModal, $scope, $http) {
+    this._$http = $http;
     this._MoviesService = MoviesService;
+    this.movies = [];
+    this.movie = "";
+    
+    this.modal = $ionicModal.fromTemplate(require('./views/form.html'), {
+      scope: $scope
+    });
+
+  }
+
+  showForm() {
+    this.modal.show();
+  }
+
+  addMovie() {
+    this._$http
+    .get(`http://www.omdbapi.com/?t=${this.movie}&y=&plot=short&r=json`)
+    .then((response) => {
+      this.movies.push(response.data);
+      this.modal.hide();
+      console.log(this.movies);
+    })
 
   }
 }
--- app/modules/movies/index.js
+++ app/modules/movies/index.js
@@ -8,6 +8,6 @@ let movies = angular.module('tiy.movies', []);
 
 movies.config(config);
 movies.controller('MoviesController', controller);
-movies.service('MoviesService');
+movies.service('MoviesService', service);
 
 export default movies;
